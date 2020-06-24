5f517bfff77723432a5bf7bb3571b93091691655
Christian Aistleitner <christian@quelltextlich.at>
2020-06-25 00:59:12 +0200

    Add basic PolyGerrit UI
    Due to lack of an extension point in the related changes list, we add
    the CRDs underneath votes. This is also where it was for the GWT UI.
    
    This basic variant of the UI shows the CRDs and provides links. But it
    does not yet color them red if cycles are detected.
    
    Change-Id: I019d3b35de2933abecb5caef0613a02904c8eb68

---
+++ gr-zuul/gr-zuul_html.js
@@ -0,0 +1,51 @@
+/**
+ * @license
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export const htmlTemplate = Polymer.html`
+    <style include="gr-change-metadata-shared-styles">
+    /* Workaround for empty style block - see https://github.com/Polymer/tools/issues/408 */
+    </style>
+    <template is="dom-if" if="[[_crd_loaded]]">
+      <template is="dom-if" if="[[_crd.depends_on.length]]">
+        <section class="zuul-dependency">
+          <span class="title">Depends on</span>
+          <span class="value">
+            <template is="dom-repeat" items="[[_crd.depends_on]]">
+              <a href="[[_computeChangeIdLinkUrl(item)]]">
+                [[item]]
+              </a>
+              <br/>
+            </template>
+          </span>
+        </section>
+      </template>
+      <template is="dom-if" if="[[_crd.needed_by.length]]">
+        <section class="zuul-dependency">
+          <span class="title">Needed by</span>
+          <span class="value">
+            <template is="dom-repeat" items="[[_crd.needed_by]]">
+              <a href="[[_computeChangeIdLinkUrl(item)]]">
+                [[item]]
+              </a>
+              <br/>
+            </template>
+          </span>
+        </section>
+      </template>
+    </template>
+`;
+
