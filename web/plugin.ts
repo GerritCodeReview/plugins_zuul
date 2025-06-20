5fec20741b480b51a2ca5e3cb8c9bf598c09ce4c
Paladox none <thomasmulhall410@yahoo.com>
2025-06-20 02:03:27 -0700

    Convert from Polymer to Lit
    Change-Id: I4486450eef72d97edb9c9bb265ed9f41b2ca688f

--- web/plugin.ts
+++ web/plugin.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2025 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,9 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import './gr-zuul.js';
 
-Gerrit.install(plugin => {
+import '@gerritcodereview/typescript-api/gerrit';
+import './gr-zuul';
+
+Gerrit?.install(plugin => {
   plugin.registerCustomComponent(
       'related-changes-section', 'gr-zuul', {slot: 'bottom'});
 });
